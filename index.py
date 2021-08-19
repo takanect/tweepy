7818437a1c6ec27bacb94736515ad2faa5f418db
takanect <87615801+takanect@users.noreply.github.com>
2021-08-19 22:33:49 +0900

    Update index.py

--- index.py
+++ index.py
@@ -5,6 +5,6 @@ from bottle import route, run
 
 @route("/")
 def hello_world():
-    return "" # ここで返す内容は何でもよい
+    return "message" # ここで返す内容は何でもよい
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
