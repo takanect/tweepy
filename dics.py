cdfcb229df1d3a92404a1e25b913d3fb1a4fba0d
takanect <87615801+takanect@users.noreply.github.com>
2021-08-19 21:12:33 +0900

    Create dics.py

---
+++ dics.py
@@ -0,0 +1,42 @@
+#botが見るVTuberのリスト
+members = ['赤井はあと', '夏色まつり']
+
+
+#YouTube Ch の Channel ID　の辞書
+#夏色まつりさんのチャンネルのURLは、https://www.youtube.com/channel/UCQ0UDLQCjY0rmuxCDE38FGg
+#channel/ 以降の部分を書く
+YT_ID_dic  = {  '赤井はあと':'UC1CfXB_kRs3C-zaeTG3oGyg', \
+                '夏色まつり':'UCQ0UDLQCjY0rmuxCDE38FGg', \
+             }
+
+
+
+#TwitterのURL
+#mobile版のURLにすること
+tw_url_dic = {  '赤井はあと' : 'https://mobile.twitter.com/akaihaato', \
+                '夏色まつり' : 'https://mobile.twitter.com/natsuiromatsuri', \
+             }
+
+#解説1で取得したtwitter idの辞書
+tw_id_dic      = {  '赤井はあと' : 998336069992001537, \
+                    '夏色まつり' : 996645451045617664, \
+                 }
+
+#https://bitly.com で短縮したチャンネルURL
+#https://www.youtube.com/channel/***********
+bitly_yt_dic = { '赤井はあと' : 'https://bit.ly/2vSwfee', \
+                 '夏色まつり' : 'https://bit.ly/2vKgcPp', \
+               }
+
+
+#解説ツイキャス
+tc_id_dic = {   '赤井はあと': '998336069992001537', \
+                '夏色まつり': '996645451045617664', \
+            }
+
+
+#https://bitly.com で短縮したツイキャスURL
+#https://twitcasting.tv/********
+bitly_tc_dic = {    '赤井はあと': 'https://bit.ly/2VaLclQ', \
+                    '夏色まつり': 'https://bit.ly/2yi3fOh', \
+               }
